074c77a831788537fb484a27cc65d2c8c2030546
P.J.Meisch <pj.meisch@sothawo.com>
2017-07-26 16:43:35 +0200

    refactored code

--- idproducer/src/main/java/com/sothawo/basy/IdProducer.java
+++ idproducer/src/main/java/com/sothawo/basy/IdProducer.java
@@ -4,7 +4,6 @@
 package com.sothawo.basy;
 
 import com.sothawo.basy.events.IdCreated;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,24 +18,17 @@ public class IdProducer {
 
     private static final long SLEEP_TIME = 10 * 1_000;
 
-    @NotNull
-    private final EventStoreProducer eventStoreProducer;
-
-    public IdProducer(@NotNull EventStoreProducer eventStoreProducer) {
-        this.eventStoreProducer = eventStoreProducer;
+    public static void main(String[] args) {
+        new IdProducer().run();
     }
 
-    public static void main(String[] args) {
+    private void run() {
         final Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "com.sothawo.basy.events.EventSerde");
 
-        new IdProducer(new KafkaEventStoreProducer(props)).run();
-    }
-
-    private void run() {
-        try {
+        try (EventStoreProducer eventStoreProducer = new KafkaEventStoreProducer(props)) {
             //noinspection InfiniteLoopStatement
             while (true) {
                 final IdCreated event = new IdCreated(UUID.randomUUID().toString());
@@ -45,7 +37,7 @@ public class IdProducer {
 
                 Thread.sleep(SLEEP_TIME);
             }
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             logger.error("exiting program", e);
         }
 
