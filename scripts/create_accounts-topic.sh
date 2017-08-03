08e4cde0052310c63903090f88d4d679d835ab5b
P.J.Meisch <pj.meisch@sothawo.com>
2017-08-03 11:30:27 +0200

    changed topic config to keep events for 1 year

--- scripts/create_accounts-topic.sh
+++ scripts/create_accounts-topic.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# retention.ms: 1 year
+
 $KAFKA_DIR/bin/kafka-topics.sh \
     --zookeeper localhost:2181 \
     --create \
     --topic basy-account-events \
     --partitions 100 \
-    --replication-factor 1
+    --replication-factor 1 \
+    --config retention.ms=31536000000
